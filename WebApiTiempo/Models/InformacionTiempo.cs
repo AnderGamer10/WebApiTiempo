bc5aec61f5446a928d229335b94197a2bc56ec1c
AnderGamer10 <anderedondo10@hotmail.com>
2022-01-19 13:12:23 +0100

    creando base de datos

--- WebApiTiempo/Models/InformacionTiempo.cs
+++ WebApiTiempo/Models/InformacionTiempo.cs
@@ -9,13 +9,15 @@ namespace WebApiTiempo.Models
     public class InformacionTiempo
     {
         [Key]
+        public string Id { get; set; }
         public string Municipio { get; set; }
-        public string ultima_hora { get; set; }
+        public string Region { get; set; }
+        public string Ultima_hora { get; set; }
         public string Temperatura { get; set; }
         public string Humedad { get; set; }
         public string Velocidad_Viento { get; set; }
         public string Precipitacion_Acumulada { get; set; }
-        public string latitud { get; set; }
-        public string longitud { get; set; }
+        public string GpxX { get; set; }
+        public string GpxY { get; set; }
     }
 }
